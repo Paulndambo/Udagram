1bcfb82c87d781fac71cbdb09860d8074cddd133
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2022-10-02 19:27:59 +0300

    server file updated

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {deleteLocalFiles, filterImageFromURL} from './util/util'
 const path = require("path");
 
 (async () => {
@@ -14,28 +14,23 @@ const path = require("path");
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  app.get("/filteredimage/", (req: Request, res: Response) => {
-    let { image_url } = req.query
-    if (!image_url) {
-      return res.status(400).send("Please pass an image url")
+app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url } = req.query;
+    try {
+      const imagePath = await filterImageFromURL(image_url);
+      console.log(imagePath);
+      res.status(200).sendFile(imagePath, (err) => {
+        if (err) {
+          res.status(500).send(err);
+          return;
+        }
+        deleteLocalFiles([imagePath]);
+      });
+    } catch (err) {
+      res.status(422).send("An error occurred");
+      return;
     }
-    let filteredpath = filterImageFromURL(image_url)
-    console.log(filteredpath)
-    return res.status(200).send(filteredpath)
   })
   /**************************************************************************** */
 
